2e3182743c3dc47d48335d59c3d019ce2d36673d
boyang1020 <boyang1020@gmail.com>
2018-09-26 20:10:46 -0700

    get the http method and query string as object

--- index.js
+++ index.js
@@ -3,10 +3,10 @@
 //  Dependencies
 
 var http = require('http');
-var url  = require('url');
+var url = require('url');
 
 // The server should respond to all requests with a string
-var server = http.createServer(function(req, res){
+var server = http.createServer(function (req, res) {
 
 
     // get url and parse it
@@ -14,17 +14,27 @@ var server = http.createServer(function(req, res){
 
     // get the path
     var path = parsedUrl.pathname;
-    var trimmedPath = path.replace(/^\/+|\/+$/g, '')
+    var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    //  Get the query string as an object
+
+    var queryStringObject = parsedUrl.query;
+
+
+
+    // Get the HTTP Method
+
+    var method = req.method.toLowerCase();
 
     // send the response 
     res.end('Hello World\n');
 
     // log the request path
-    console.log('Request reveived on path: '+trimmedPath);
-    
+    console.log('Request reveived on path: ' + trimmedPath + ' with method : ' + method + ' and with these query string parameters', queryStringObject );
+
 });
 // Start the server, and have it  listen on port 3000
 
-server.listen(3000, function(){
+server.listen(3000, function () {
     console.log('The server is listening on port 3000 now');
 });
